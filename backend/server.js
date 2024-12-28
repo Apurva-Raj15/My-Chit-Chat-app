fdeb5355e1048bb0f46c55671be3b37f26b51b92
Apurva <shivangi.apurva15@gmail.com>
2024-12-28 20:49:48 +0530

    2nd part completed - uidesign

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,36 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
 import userRoutes from "./routes/user.routes.js";
 
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
+dotenv.config();
+
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables.
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-dotenv.config(); // Load .env variables
 
-
-
-app.use(express.json());
+app.use(express.json()); // To parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// Default error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "Something went wrong!" });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-// Start the server
-app.listen(PORT, async () => {
-    await connectToMongoDB();
-    console.log(`Server Running on port ${PORT}`);
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
