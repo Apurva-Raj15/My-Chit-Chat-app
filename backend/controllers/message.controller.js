8d751fba78461aaca444e49f65087d707d2e6bcf
Apurva <shivangi.apurva15@gmail.com>
2024-12-31 01:02:03 +0530

    Deployment

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,48 +1,52 @@
+
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+
 export const sendMessage = async (req, res) => {
-    try {
-        const { message } = req.body;
-        const { id: receiverId } = req.params;
-        const senderId = req.user._id;
+	try {
+		const { message } = req.body;
+		const { id: receiverId } = req.params;
+		const senderId = req.user._id;
 
-        // Check if a conversation already exists between the participants
-        let conversation = await Conversation.findOne({
-            participants: { $all: [senderId, receiverId] },
-        });
+		let conversation = await Conversation.findOne({
+			participants: { $all: [senderId, receiverId] },
+		});
 
-        // If no conversation exists, create a new one
-        if (!conversation) {
-            conversation = await Conversation.create({
-                participants: [senderId, receiverId],
-                messages: [], // Initialize messages array
-            });
-        }
+		if (!conversation) {
+			conversation = await Conversation.create({
+				participants: [senderId, receiverId],
+			});
+		}
 
-        // Create the new message
-        const newMessage = new Message({
-            senderId,
-            receiverId,
-            message,
-        });
+		const newMessage = new Message({
+			senderId,
+			receiverId,
+			message,
+		});
 
-        if (newMessage) {
+		if (newMessage) {
 			conversation.messages.push(newMessage._id);
 		}
 
-        // Add the message to the conversation
-    //    await conversation.save();
-    //    await newMessage.save();
-    
+		// await conversation.save();
+		// await newMessage.save();
+
 		// this will run in parallel
 		await Promise.all([conversation.save(), newMessage.save()]);
 
-        // Respond with the new message
-        res.status(201).json(newMessage);
-    } catch (error) {
-        console.error("Error in sendMessage:", error.message);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+		// SOCKET IO FUNCTIONALITY WILL GO HERE
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
+
+		res.status(201).json(newMessage);
+	} catch (error) {
+		console.log("Error in sendMessage controller: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
 };
 
 export const getMessages = async (req, res) => {
@@ -64,4 +68,3 @@ export const getMessages = async (req, res) => {
 		res.status(500).json({ error: "Internal server error" });
 	}
 };
-
--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,20 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-
-
-
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
 const __dirname = path.resolve();
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables.
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
-const app = express();
-
-
-app.use(express.json()); // To parse the incoming requests with JSON payloads (from req.body)
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -31,10 +26,10 @@ app.use("/api/users", userRoutes);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
-  connectToMongoDB();
-  console.log(`Server Running on port ${PORT}`);
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
